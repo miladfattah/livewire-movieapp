d3bbd745684bcc4e7c4a66a68abfc80b724a5b92
MiladFattah <miladalpha1379@gmail.com>
2022-04-03 23:33:20 -0700

    create movies index page

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,6 +7,12 @@ use App\Models\Movie;
 
 class MovieController extends Controller
 {
+    public function index()
+    {
+        $movies = Movie::orderBy('created_at' , 'desc')->paginate(8);
+        return view('movie.index' , compact('movies'));
+    }   
+
     public function show(Movie $movie)
     {
         $latest = Movie::orderBy('created_at' , 'desc')->take(9)->get();
